94781d8e257d13b962ee274bb80504b17917af97
nansaidh <melanie.wolf_3008@t-online.de>
2020-09-12 16:33:44 +0200

    code cleanup

--- src/demo_old.py
+++ src/demo_old.py
@@ -12,6 +12,7 @@ from trt_pose.draw_objects import DrawObjects
 from trt_pose.parse_objects import ParseObjects
 
 import argparse
+from os import path
 
 
 DIR_DATASETS = '../datasets/'
@@ -25,65 +26,11 @@ OPTIMIZED_MODEL_RESNET18 = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
 WIDTH = 224
 HEIGHT = 224
 
-'''
-hnum: 0 based human index
-kpoint : keypoints (float type range : 0.0 ~ 1.0 ==> later multiply by image width, height
-'''
-def get_keypoint(humans, hnum, peaks):
-    #check invalid human index
-    kpoint = []
-    human = humans[0][hnum]
-    C = human.shape[0]
-    for j in range(C):
-        k = int(human[j])
-        if k >= 0:
-            peak = peaks[0][j][k]   # peak[1]:width, peak[0]:height
-            peak = (j, float(peak[0]), float(peak[1]))
-            kpoint.append(peak)
-        else:
-            peak = (j, None, None)
-            kpoint.append(peak)
-    return kpoint
-
 parser = argparse.ArgumentParser(description='TensorRT pose estimation run')
 parser.add_argument('--video', type=str, default='video.mp4')
 parser.add_argument('--path', type=str, default='/videos/')
 args = parser.parse_args()
 
-with open(DIR_DATASETS + DATASET, 'r') as f:
-    human_pose = json.load(f)
-
-topology = trt_pose.coco.coco_category_to_topology(human_pose)
-
-num_parts = len(human_pose['keypoints'])
-num_links = len(human_pose['skeleton'])
-
-model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
-
-model.load_state_dict(torch.load(DIR_PRETRAINED_MODELS + MODEL_RESNET18))
-
-data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
-
-model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
-torch.save(model_trt.state_dict(), DIR_PRETRAINED_MODELS + OPTIMIZED_MODEL_RESNET18)
-
-print('Model optimized')
-
-model_trt = TRTModule()
-model_trt.load_state_dict(torch.load(DIR_PRETRAINED_MODELS + OPTIMIZED_MODEL_RESNET18))
-
-t0 = time.time()
-torch.cuda.current_stream().synchronize()
-for i in range(50):
-    y = model_trt(data)
-torch.cuda.current_stream().synchronize()
-t1 = time.time()
-
-print(50.0 / (t1 - t0))
-
-mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
-std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
-device = torch.device('cuda')
 
 def preprocess(image):
     global device
@@ -95,35 +42,74 @@ def preprocess(image):
     return image[None, ...]
 
 
-parse_objects = ParseObjects(topology)
-draw_objects = DrawObjects(topology)
+def initialize_video_writer():
+    print('initialize video capture')
+    cap = cv2.VideoCapture(args.path + args.video)
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    frame_size = (frame_width, frame_height)
+    print('initialize video writer')
+    out_video = cv2.VideoWriter(args.video_path + 'output.mp4', fourcc, cap.get(cv2.CAP_PROP_FPS), frame_size)
 
-cap = cv2.VideoCapture(args.path+args.video)
+    return cap, out_video
 
-print('Video captured')
-
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-out_video = cv2.VideoWriter(args.path+'output.mp4', fourcc, cap.get(cv2.CAP_PROP_FPS), (int(cap.get(3)), int(cap.get(4))))
-X_compress = int(cap.get(3)) / WIDTH * 1.0
-Y_compress = int(cap.get(4)) / HEIGHT * 1.0
-
-print('out_video')
-
+def clean_up():
+    cv2.destroyAllWindows()
+    out_video.release()
+    cap.release()
+    print('all released')
 
 def execute(image, src, tm, out_vid):
     img_data = preprocess(image)
     cmap, paf = model_trt(img_data)
     cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
-    counts, objects, peaks = parse_objects(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
+    counts, objects, peaks = parse_objects(cmap, paf)
     fps = 1.0 / (time.time() - tm)
     print("FPS:%f " % fps)
     draw_objects(src, counts, objects, peaks)
-    cv2.putText(src, "FPS: %f" % fps, (20, 30),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+    cv2.putText(src, "FPS: %f" % fps, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
     out_vid.write(src)
 
 
-count = 0
+with open(DIR_DATASETS + DATASET, 'r') as f:
+    human_pose = json.load(f)
 
+topology = trt_pose.coco.coco_category_to_topology(human_pose)
+
+num_parts = len(human_pose['keypoints'])
+num_links = len(human_pose['skeleton'])
+
+model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
+
+data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+
+if not path.exists(DIR_PRETRAINED_MODELS + OPTIMIZED_MODEL_RESNET18):
+    model.load_state_dict(torch.load(DIR_PRETRAINED_MODELS + MODEL_RESNET18))
+    model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+    torch.save(model_trt.state_dict(), DIR_PRETRAINED_MODELS + OPTIMIZED_MODEL_RESNET18)
+    print('Model optimized')
+
+model_trt = TRTModule()
+model_trt.load_state_dict(torch.load(DIR_PRETRAINED_MODELS + OPTIMIZED_MODEL_RESNET18))
+
+#t0 = time.time()
+#torch.cuda.current_stream().synchronize()
+#for i in range(50):
+#    y = model_trt(data)
+#torch.cuda.current_stream().synchronize()
+#t1 = time.time()
+
+#print(50.0 / (t1 - t0))
+
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+device = torch.device('cuda')
+
+parse_objects = ParseObjects(topology)
+draw_objects = DrawObjects(topology)
+
+cap, out_video = initialize_video_writer()
 
 while cap.isOpened():
     t = time.time()
@@ -141,9 +127,6 @@ while cap.isOpened():
     execute(img, frame, t, out_video)
     print('Started execution')
 
-cv2.destroyAllWindows()
-print('Windows destroyed')
-out_video.release()
-cap.release()
-print('all released')
+clean_up()
+print("Process finished")
 
